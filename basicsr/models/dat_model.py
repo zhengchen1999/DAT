0a843c2645645ca1c2e8e24ef4c020ebf2f14f08
463465810cz <48750880+zhengchen1999@users.noreply.github.com>
2023-10-25 20:54:53 +0800

    update model

--- basicsr/models/dat_model.py
+++ basicsr/models/dat_model.py
@@ -6,7 +6,7 @@ from basicsr.models.sr_model import SRModel
 
 
 @MODEL_REGISTRY.register()
-class DATModle(SRModel):
+class DATModel(SRModel):
 
     def test(self):
         self.use_chop = self.opt['val']['use_chop'] if 'use_chop' in self.opt['val'] else False
